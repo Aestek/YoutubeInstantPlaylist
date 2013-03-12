abdcfc967572502bd2bf83b04107a5556942d4cc
Aestek <thib.gilles@gmail.com>
2013-03-12 22:31:48 +0100

    bugfix

--- public/javascripts/app-controllers.js
+++ public/javascripts/app-controllers.js
@@ -13,8 +13,9 @@ function MainCtrl($scope, videoStore) {
 		elapsedTime: 0,
 		currentElapsedTime: 0,
 		playing: false,
-		playerState: -1,
-		volume: 80
+		playerState: 5,
+		volume: 80,
+		mute: false
 	};
 
 	// methods
@@ -31,9 +32,8 @@ function MainCtrl($scope, videoStore) {
 	};
 
 	$scope.setIndex = function(i) {
-		if (i < $scope.playlist.items.length && i >= 0) {
+		if (i < $scope.playlist.items.length && i >= 0)
 			$scope.playback.position = i;
-		}
 		else
 			$scope.playback.position = -1;
 	};
@@ -62,8 +62,8 @@ function MainCtrl($scope, videoStore) {
 
 	// watcher
 
-	$scope.$watch('playback.position', function() {
-
+	$scope.$watch('playback.position', function(val) {
+		console.log(val)
 		$scope.playback.currentVideo = ($scope.playlist.items[$scope.playback.position] || {}).video || {};
 
 		if (!$scope.playback.currentVideo.relatedVideos && $scope.playback.currentVideo.id)
@@ -86,15 +86,40 @@ function MainCtrl($scope, videoStore) {
 
 	$scope.$watch('playlist', function() {
 		var found = false;
+		var duration = 0;
+		var currentDuration = 0;
 		for (var i in $scope.playlist.items) {
 			if ($scope.playlist.items[i].playing) {
 				$scope.setIndex(parseInt(i));
 				found = true;
 			}
+			if (!found)
+				currentDuration += $scope.playlist.items[i].video.duration;
+			duration += $scope.playlist.items[i].video.duration;
 		}
-
 		if (!found)
-			$scope.playback.position = $scope.setIndex(-1);
+			$scope.setIndex();
+
+		$scope.playlist.totalTime = duration;
+		$scope.playback.elapsedTime = currentDuration;
 	}, true);
 }
 
+function PlayerControlsCtrl($scope) {
+	$scope.togglePlay = function() {
+		if ($scope.playback.currentVideo.id)
+			$scope.playback.playerState = $scope.playback.playerState == 1 ? 2 : 1;
+	};
+
+	$scope.next = function() {
+		$scope.setIndex($scope.playback.position + 1);
+	};
+
+	$scope.previous = function() {
+		$scope.setIndex($scope.playback.position - 1);
+	};
+
+	$scope.$watch('playback.volume', function() {
+		$scope.playback.mute = false;
+	});
+}
--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -33,7 +33,8 @@ app.directive('youtubePlayer', function() {
       		video: '=',
      		currentTime: '=',
      		playerState: '=',
-     		volume: '='
+     		volume: '=',
+     		mute: '='
      	},
      	controller: function($scope, $element, guid) {
      		var $el = $($element);
@@ -78,7 +79,6 @@ app.directive('youtubePlayer', function() {
 		    	player.addEventListener('onStateChange', 'onYouTubePlayerStateChange');
 
 			    $scope.$watch('video', function() {
-			    	console.log('video', $scope.video)
 			    	if ($scope.video.id) {
 			    		switchPlayerVisibility(true);
 			    		player.loadVideoById($scope.video.id);
@@ -92,6 +92,20 @@ app.directive('youtubePlayer', function() {
 
 			    $scope.$watch('volume', function(val) {
 			    	player.setVolume(val);
+			    });
+
+			    $scope.$watch('playerState', function(val) {
+			    	if (val == 1)
+			    		player.playVideo();
+			    	else if (val == 2)
+			    		player.pauseVideo();
+			    });
+
+			    $scope.$watch('mute', function(val) {
+			    	if (val)
+			    		player.mute();
+			    	else
+			    		player.unMute();
 			    });
 			};
 
@@ -321,3 +335,105 @@ app.directive('verticalProgressBar', function() {
      	}
     };
 });
+
+app.directive('bsPopover', ['$parse', '$compile', '$http', '$timeout', '$q', '$templateCache', function($parse, $compile, $http, $timeout, $q, $templateCache) {
+  'use strict';
+
+  // Hide popovers when pressing esc
+  $("body").on("keyup", function(ev) {
+    if(ev.keyCode === 27) {
+      $(".popover.in").each(function() {
+        $(this).popover('hide');
+      });
+    }
+  });
+
+  return {
+    restrict: 'A',
+    scope: true,
+    link: function postLink(scope, element, attr, ctrl) {
+
+      var getter = $parse(attr.bsPopover),
+        setter = getter.assign,
+        value = getter(scope),
+        options = {};
+
+      if(angular.isObject(value)) {
+        options = value;
+        options.content = '<div back-img="{{playlist.items[playback.position - 1].thumbnail.hqDefault}}" />'
+      }
+
+      $q.when(options.content || $templateCache.get(value) || $http.get(value, {cache: true})).then(function onSuccess(template) {
+
+        // Handle response from $http promise
+        if(angular.isObject(template)) {
+          template = template.data;
+        }
+
+        // Handle data-unique attribute
+       // if(!!attr.unique) {
+          element.on('show', function(ev) { // requires bootstrap 2.3.0+
+            // Hide any active popover except self
+            $(".popover.in").each(function() {
+              var $this = $(this),
+                popover = $this.data('popover');
+              if(popover && !popover.$element.is(element)) {
+                $this.popover('hide');
+              }
+            });
+          });
+        //}
+
+        // Handle data-hide attribute to toggle visibility
+        if(!!attr.hide) {
+          scope.$watch(attr.hide, function(newValue, oldValue) {
+            if(!!newValue) {
+              popover.hide();
+            } else if(newValue !== oldValue) {
+              popover.show();
+            }
+          });
+        }
+
+        // Initialize popover
+        element.popover(angular.extend({}, options, {
+          content: template,
+          html: true
+        }));
+
+        // Bootstrap override to provide tip() reference & compilation
+        var popover = element.data('popover');
+        popover.hasContent = function() {
+          return this.getTitle() || template; // fix multiple $compile()
+        };
+        popover.getPosition = function() {
+          var r = $.fn.popover.Constructor.prototype.getPosition.apply(this, arguments);
+
+          // Compile content
+          $compile(this.$tip)(scope);
+          scope.$digest();
+
+          // Bind popover to the tip()
+          this.$tip.data('popover', this);
+
+          return r;
+        };
+
+        // Provide scope display functions
+        scope._popover = function(name) {
+          element.popover(name);
+        };
+        scope.hide = function() {
+          element.popover('hide');
+        };
+        scope.show = function() {
+          element.popover('show');
+        };
+        scope.dismiss = scope.hide;
+
+      });
+
+    }
+  };
+
+}]);
